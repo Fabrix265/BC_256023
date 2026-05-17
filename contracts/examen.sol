add7a003896bcf87c996ad2fa4980e34c1f73851
Fabrix265 <fabrix336@gmail.com>
2026-05-16 20:24:41 -0500

    Parte 3

--- contracts/examen.sol
+++ contracts/examen.sol
@@ -9,23 +9,33 @@ contract StreamingMusical256023 {
         uint256 id;
         string titulo;
         uint256 duracion;
+        bool estado;
     }
 
     Cancion[] public canciones;
     address public dirContrato;
+
+    modifier registrarEjecucion() {
+        console.log("Ejecutado por: 256023 - Fabrizio Vidal Rodriguez");
+        _;
+    }
 
     constructor() {
         console.log("Ejecutado por: 256023 - Fabrizio Vidal Rodriguez");
         dirContrato = address(this);
     }
 
-    function agregarElemento(uint256 _id, string memory _titulo, uint256 _duracion) public {
-        console.log("Ejecutado por: 256023 - Fabrizio Vidal Rodriguez");
-        canciones.push(Cancion(_id, _titulo, _duracion));
+    function agregarElemento(uint256 _id, string memory _titulo, uint256 _duracion, bool _estado) public registrarEjecucion {
+        require(_duracion > 0, "La duracion debe ser mayor a cero segundos");
+
+        for (uint256 i = 0; i < canciones.length; i++) {
+            require(canciones[i].id != _id, "El ID de la cancion ya existe");
+        }
+
+        canciones.push(Cancion(_id, _titulo, _duracion, _estado));
     }
 
-    function contarElementos() public view returns (uint256) {
-        console.log("Ejecutado por: 256023 - Fabrizio Vidal Rodriguez");
+    function contarElementos() public view registrarEjecucion returns (uint256) {
         return canciones.length;
     }
 }
